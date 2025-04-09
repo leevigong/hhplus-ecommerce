1815e8e757885ebcaa858b33ca39bd50860ebcfb
leevigong <dddddaeun88.88@gmail.com>
2025-04-09 23:16:58 +0900

    feat: User 도메인(엔티티) 작성

--- src/main/java/kr/hhplus/be/server/global/exception/ApiErrorCode.java
+++ src/main/java/kr/hhplus/be/server/global/exception/ApiErrorCode.java
@@ -7,6 +7,10 @@ import org.springframework.http.HttpStatus;
 public enum ApiErrorCode {
 
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."),
+
+    // User
+    NOT_FOUND_USER(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
+    INVALID_NICKNAME(HttpStatus.BAD_REQUEST, "유효하지 않은 닉네임입니다."),
     ;
 
     private final HttpStatus httpStatus;
