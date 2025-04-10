88af995c28a2ed6b60facfc045b49b0b8e373d3b
leevigong <dddddaeun88.88@gmail.com>
2025-04-11 04:05:33 +0900

    refactor: @Repository 제거

--- src/main/java/kr/hhplus/be/server/domain/product/ProductSalesRankRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/product/ProductSalesRankRepository.java
@@ -1,11 +1,9 @@
 package kr.hhplus.be.server.domain.product;
 
 import kr.hhplus.be.server.domain.product.enums.RankingScope;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface ProductSalesRankRepository {
 
     List<ProductSalesRank> findByRankingScope(RankingScope rankingScope);
