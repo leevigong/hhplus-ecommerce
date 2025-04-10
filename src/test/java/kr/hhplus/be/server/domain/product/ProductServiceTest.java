90df21b61decf54c8ba5b6f3773b74bdb46c8ff4
leevigong <dddddaeun88.88@gmail.com>
2025-04-10 11:15:31 +0900

    test: Product 서비스 단위 테스트 작성

---
+++ src/test/java/kr/hhplus/be/server/domain/product/ProductServiceTest.java
@@ -0,0 +1,35 @@
+package kr.hhplus.be.server.domain.product;
+
+import kr.hhplus.be.server.domain.product.enums.Category;
+import kr.hhplus.be.server.inferfaces.product.dto.ProductResponse;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+
+    @Mock
+    private ProductRepository productRepository;
+    @InjectMocks
+    private ProductService getProductService;
+
+    @Test
+    void 상품_아이디로_상품_조회_성공() {
+        // given
+        Product product = new Product(1L, "상의", 10000, 10, Category.TOP);
+        when(productRepository.findById(1L)).thenReturn(product);
+
+        // when
+        ProductResponse.ProductV1 response = getProductService.getProductById(1L);
+
+        // then
+        assertEquals("상의", response.getProductName());
+        assertEquals(10000, response.getPrice());
+    }
+}
