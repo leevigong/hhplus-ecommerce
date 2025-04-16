2a14797c6fba73f17b9bdd5374324afdb991bc51
leevigong <dddddaeun88.88@gmail.com>
2025-04-16 18:46:56 +0900

    chore: 통합테스트를 위한 설정 추가 및 변경

--- src/test/java/kr/hhplus/be/server/ServerApplicationTests.java
+++ src/test/java/kr/hhplus/be/server/ServerApplicationTests.java
@@ -1,14 +1,13 @@
-//package kr.hhplus.be.server;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.context.annotation.Import;
-//
-//@SpringBootTest
-//class ServerApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package kr.hhplus.be.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
