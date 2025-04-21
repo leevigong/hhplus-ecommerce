df1ce9f3dd33bc1c74a79cbac6e86bff0888e524
leevigong <dddddaeun88.88@gmail.com>
2025-04-22 00:42:42 +0900

    feat: 주문 확정 후 데이터 플랫폼에 전송 로직 추가

--- src/main/java/kr/hhplus/be/server/application/order/OrderFacade.java
+++ src/main/java/kr/hhplus/be/server/application/order/OrderFacade.java
@@ -1,42 +1,42 @@
 package kr.hhplus.be.server.application.order;
 
+import kr.hhplus.be.server.application.order.port.OrderDataPlatformClient;
 import kr.hhplus.be.server.application.payment.PaymentCriteria;
-import kr.hhplus.be.server.application.payment.PaymentFacade;
 import kr.hhplus.be.server.domain.balance.UserBalanceCommand;
 import kr.hhplus.be.server.domain.balance.UserBalanceService;
-import kr.hhplus.be.server.domain.coupon.UserCouponService;
 import kr.hhplus.be.server.domain.order.OrderCommand;
 import kr.hhplus.be.server.domain.order.OrderInfo;
 import kr.hhplus.be.server.domain.order.OrderService;
 import kr.hhplus.be.server.domain.payment.PaymentService;
 import kr.hhplus.be.server.domain.product.ProductService;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
-@Transactional
 public class OrderFacade {
 
     private final OrderService orderService;
     private final ProductService productService;
     private final PaymentService paymentService;
     private final UserBalanceService userBalanceService;
+    private final OrderDataPlatformClient orderDataPlatformClient;
 
     public OrderFacade(OrderService orderService,
                        ProductService productService,
                        PaymentService paymentService,
-                       UserBalanceService userBalanceService) {
+                       UserBalanceService userBalanceService,
+                       OrderDataPlatformClient orderDataPlatformClient) {
         this.orderService = orderService;
         this.productService = productService;
         this.paymentService = paymentService;
         this.userBalanceService = userBalanceService;
+        this.orderDataPlatformClient = orderDataPlatformClient;
     }
 
     @Transactional
     public OrderResult order(OrderCriteria.Create criteria) {
-
         OrderCommand.Create createCommand = criteria.toCommand();
+
         // product 검증 및 재고 감소
         productService.validateAndSubStockProducts(createCommand.getCreateOrderItems());
 
@@ -51,7 +51,10 @@ public class OrderFacade {
         userBalanceService.use(UserBalanceCommand.Use.of(paymentCriteria.userId(), paymentCriteria.finalPrice()));
 
         // 주문 확정
-        orderService.confirmOrder(OrderCommand.Confirm.from(paymentCriteria.orderId()));
+        orderInfo = orderService.confirmOrder(OrderCommand.Confirm.from(paymentCriteria.orderId()));
+
+        // 데이터 플랫폼에 전송
+        orderDataPlatformClient.sendOrderData(orderInfo);
 
         return OrderResult.from(orderInfo);
     }
