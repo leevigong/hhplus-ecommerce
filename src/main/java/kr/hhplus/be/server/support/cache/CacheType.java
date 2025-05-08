4a1124669481db186418b32dfe716b49b0df6cad
leevigong <dddddaeun88.88@gmail.com>
2025-05-09 02:29:31 +0900

    feat: 공통 캐시 인터페이스 도입 및 캐시 타입 정의

---
+++ src/main/java/kr/hhplus/be/server/support/cache/CacheType.java
@@ -0,0 +1,21 @@
+package kr.hhplus.be.server.support.cache;
+
+import lombok.Getter;
+
+import java.time.Duration;
+
+@Getter
+public enum CacheType implements Cacheable {
+
+    POPULAR_PRODUCTS(CacheNames.POPULAR_PRODUCTS, Duration.ofHours(25)),
+    PRODUCT(CacheNames.PRODUCT, Duration.ofDays(7))
+    ;
+
+    private final String cacheName;
+    private final Duration ttl;
+
+    CacheType(String cacheName, Duration ttl) {
+        this.cacheName = cacheName;
+        this.ttl = ttl;
+    }
+}
