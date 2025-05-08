44eee49384c55481874f7cd54594887a1f6cf5eb
leevigong <dddddaeun88.88@gmail.com>
2025-05-09 01:47:21 +0900

    feat: 분산락 AOP 적용

--- src/main/java/kr/hhplus/be/server/application/coupon/UserCouponFacade.java
+++ src/main/java/kr/hhplus/be/server/application/coupon/UserCouponFacade.java
@@ -5,31 +5,23 @@ import kr.hhplus.be.server.domain.coupon.CouponService;
 import kr.hhplus.be.server.domain.userCoupon.UserCouponCommand;
 import kr.hhplus.be.server.domain.userCoupon.UserCouponInfo;
 import kr.hhplus.be.server.domain.userCoupon.UserCouponService;
-import kr.hhplus.be.server.support.exception.ApiErrorCode;
-import kr.hhplus.be.server.support.exception.ApiException;
-import lombok.extern.slf4j.Slf4j;
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
+import kr.hhplus.be.server.support.lock.DistributedLock;
+import kr.hhplus.be.server.support.lock.LockResource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Service
-@Slf4j
 public class UserCouponFacade {
 
     private final CouponService couponService;
     private final UserCouponService userCouponService;
-    private final RedissonClient redisson;
 
-    public UserCouponFacade(CouponService couponService, UserCouponService userCouponService, RedissonClient redisson, TransactionTemplate tx) {
+    public UserCouponFacade(CouponService couponService, UserCouponService userCouponService) {
         this.couponService = couponService;
         this.userCouponService = userCouponService;
-        this.redisson = redisson;
     }
 
     @Transactional(readOnly = true)
@@ -41,27 +33,14 @@ public class UserCouponFacade {
         return UserCouponResult.Coupons.of(userCouponIds);
     }
 
+    @DistributedLock(resource = LockResource.USER_COUPON, key = "#criteria.couponId")
+    @Transactional
     public void issue(UserCouponCriteria criteria) {
-        RLock lock = redisson.getLock("userCoupon:issue:" + criteria.couponId());
-        log.info("lock: {}", lock.getName());
+        // 쿠폰 발급
+        CouponInfo couponInfo = couponService.issueCoupon(criteria.toCommand());
 
-        boolean acquired = false;
-        try {
-            acquired = lock.tryLock(10, 20, TimeUnit.SECONDS);
-            if (!acquired) {
-                throw new ApiException(ApiErrorCode.COUPON_ISSUE_IN_PROGRESS);
-            }
-
-            // 쿠폰 발급
-            CouponInfo couponInfo = couponService.issueCoupon(criteria.toCommand());
-
-            // 사용자 쿠폰 생성
-            userCouponService.createUserCoupon(UserCouponCommand.of(couponInfo.coupon(), criteria.userId()));
-
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-        } finally {
-            if (acquired && lock.isHeldByCurrentThread()) lock.unlock();
-        }
+        // 사용자 쿠폰 생성
+        userCouponService.createUserCoupon(UserCouponCommand.of(couponInfo.coupon(), criteria.userId()));
     }
+
 }
