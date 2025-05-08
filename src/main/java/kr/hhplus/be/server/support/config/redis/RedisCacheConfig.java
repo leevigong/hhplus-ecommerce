82c1471a337e0d4d4933cca2db75956b05fb8d00
leevigong <dddddaeun88.88@gmail.com>
2025-05-09 02:31:18 +0900

    feat: Redis 캐시 직렬화 및 CacheType 기반 TTL 설정 추가

---
+++ src/main/java/kr/hhplus/be/server/support/config/redis/RedisCacheConfig.java
@@ -0,0 +1,45 @@
+package kr.hhplus.be.server.support.config.redis;
+
+import kr.hhplus.be.server.support.cache.CacheType;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Configuration
+@EnableCaching
+public class RedisCacheConfig {
+
+    /**
+     * Spring Cache용 RedisCacheManager
+     */
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory connectionFactory) {
+
+        // 직렬화
+        RedisCacheConfiguration defaultConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeKeysWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(RedisSerializer.string()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .disableCachingNullValues();
+
+        // CacheType → 개별 TTL 매핑
+        Map<String, RedisCacheConfiguration> cacheConfigMap = Arrays.stream(CacheType.values())
+                .collect(Collectors.toMap(CacheType::getCacheName, ct -> defaultConfig.entryTtl(ct.getTtl())));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(defaultConfig)
+                .withInitialCacheConfigurations(cacheConfigMap)
+                .build();
+    }
+}
