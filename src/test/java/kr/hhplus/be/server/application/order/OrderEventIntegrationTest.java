da1b7fc97ba340b4ad574b0c7ca328b7fcf03e37
leevigong <dddddaeun88.88@gmail.com>
2025-05-29 14:22:57 +0900

    test: 이벤트 카프카로 처리한 테스트

--- src/test/java/kr/hhplus/be/server/application/order/OrderEventIntegrationTest.java
+++ src/test/java/kr/hhplus/be/server/application/order/OrderEventIntegrationTest.java
@@ -1,13 +1,17 @@
 package kr.hhplus.be.server.application.order;
 
+import kr.hhplus.be.server.application.order.event.OrderEventKafkaConsumer;
 import kr.hhplus.be.server.application.order.port.OrderDataPlatformClient;
 import kr.hhplus.be.server.domain.order.OrderInfo;
 import kr.hhplus.be.server.domain.order.event.OrderConfirmedEvent;
 import kr.hhplus.be.server.domain.product.Product;
 import kr.hhplus.be.server.domain.product.ProductRepository;
 import kr.hhplus.be.server.domain.sales.ProductSalesService;
+import kr.hhplus.be.server.infra.order.OrderEventKafkaPublisher;
 import kr.hhplus.be.server.infra.order.OrderEventSpringPublisher;
+import kr.hhplus.be.server.support.contanier.TestContainerSupport;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -24,10 +28,16 @@ import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class OrderEventIntegrationTest {
+class OrderEventIntegrationTest extends TestContainerSupport {
 
     @MockitoSpyBean
     private OrderEventSpringPublisher publisher;
+
+    @MockitoSpyBean
+    private OrderEventKafkaPublisher kafkaPublisher;
+
+    @MockitoSpyBean
+    private OrderEventKafkaConsumer kafkaConsumer;
 
     @Autowired
     private OrderFacade orderFacade;
@@ -53,6 +63,7 @@ class OrderEventIntegrationTest {
     }
 
     @Test
+    @Disabled
     void 주문_확정_후_OrderConfirmedEvent_발행_및_리스너_호출() {
         // given
         OrderCriteria.OrderItem orderItem = OrderCriteria.OrderItem.of(product.getId(), 2, product.getPrice());
@@ -71,5 +82,24 @@ class OrderEventIntegrationTest {
             verify(orderDataPlatformClient, times(1)).sendOrderData(any(OrderInfo.class));
             verify(productSalesService, times(1)).add(any());
         });
+    }
+
+    @Test
+    void 주문_확정_후_OrderConfirmedEvent_카프카_처리() {
+        // given
+        OrderCriteria.OrderItem orderItem = OrderCriteria.OrderItem.of(product.getId(), 2, product.getPrice());
+        OrderCriteria.Create criteria = OrderCriteria.Create.of(1L, List.of(orderItem), null);
+
+        // when
+        orderFacade.order(criteria);
+
+        // then
+        // 카프카 발행(퍼블리셔) 검증
+        verify(kafkaPublisher, times(1))
+                .publish(any(OrderConfirmedEvent.class));
+
+        // 카프카 리스너(컨슈머) 검증
+        await().atMost(5, SECONDS).untilAsserted(() ->
+                verify(kafkaConsumer, times(1)).listen(any(OrderConfirmedEvent.class)));
     }
 }
